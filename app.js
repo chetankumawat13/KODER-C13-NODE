913a5e0aa361e334875ac795ace529c65666ddf1
chetankumawat13 <kumawatchetan2025@gmail.com>
2025-10-29 15:19:25 +0530

    done

---
+++ app.js
@@ -0,0 +1,84 @@
+//let n = 5;
+ //for(let i = 0; i<n; i++) {
+ //   console.log("hello", i)
+ //}
+
+
+ /*function getter(inp, get = 1){
+    let cloned = [];
+    for(let i =0; i<get; i++){
+        cloned.push(inp[i])
+    }
+    
+    return cloned
+
+ }
+
+ console.log(getter([1,2,34,5],));*/
+
+
+
+ /*function joiner(inp){
+    return inp.join("+");
+
+ }
+
+ console.log(joiner(["apple","banana","dalbati","cat"]));*/
+
+
+ /*const names = ["chetan","mohit","rohit","sohitl","dada"];
+
+ let newArr = names.map(function(value){
+   return value + "ji";
+   
+ })*/
+
+const info = require("./fruits");
+console.log(info[0].name);
+const math = require("./math");
+console.log(math.g)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ math.js
@@ -0,0 +1,15 @@
+const sum = (a,b) => a+b;
+const mul = (a,b) => a*b;
+const g = 9.88;
+
+
+let obj ={
+    sum:sum,
+    mul:mul,
+    g:g,
+}
+
+module.exports = obj;
+
+
+
